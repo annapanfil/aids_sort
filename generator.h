5b367e84a06b2b0a3b6fa298ecc97638e0e0dccc
Anna <134panka@gmail.com>
2020-03-14 15:11:00 +0100

    add generator

---
+++ generator.h
@@ -0,0 +1,10 @@
+#ifndef __GENERATOR_H_INCLUDED__   // if x.h hasn't been included yet...
+#define __GENERATOR_H_INCLUDED__   //   #define this so the compiler knows it has been included
+
+int* mgenerate(int*, int, char);
+/*numbers - wskażnik do tablicy, w której mają pojawić się liczby
+n - jej rozmiar
+shape - r-random, a-ascending, d-descending, c-constant, v-vshaped
+*/
+
+#endif
