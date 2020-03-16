3abbb2735ebeedf8c7c24c38d3da5cb6de7ec9d7
MichalRasinski <michal.rasinski@icloud.com>
2020-03-16 19:23:11 +0100

    Merge sort

--- data_export.h
+++ data_export.h
@@ -19,3 +19,18 @@ void exportDataToFile(int* arr, int n, void (*sort)(int* arr, int n), char* file
 	}
 	else cout << "Unable to open file";
 }
+
+void exportDataToFile(int* arr, int n, void (*sort)(int* arr, int low, int n), char* filename) {
+	ofstream myfile(filename, ofstream::app);
+
+	auto start = chrono::high_resolution_clock::now();
+	sort(arr, 0, n-1);
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+
+	if (myfile.is_open())
+	{
+		myfile << n << "\t" << duration.count() << endl;
+	}
+	else cout << "Unable to open file";
+}
