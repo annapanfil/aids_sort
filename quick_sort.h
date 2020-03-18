9044133312f13ddd8c5548e9d2c84bb53afcdfc5
Anna <134panka@gmail.com>
2020-03-18 14:43:27 +0100

    various pivot choices

--- quick_sort.h
+++ quick_sort.h
@@ -2,16 +2,19 @@
 #include <iostream>
 using namespace std;
 
-char a;
-
 int partition(int* tab, int left, int right)
 {
-  int piv = tab[int((left+right)/2)]; //element, z którym porównujemy
+  //RÓŻNE MOŻLIWOŚCI WYBORU PIVOTA
+  int choice = int((left+right)/2);
+  // int choice = right;
+  // int choice = rand()%(right-left + 1) + left;
+
+  int piv = tab[choice]; //element, z którym porównujemy
+
+
   int i = left;                      //przesuwa się w prawo, szuka elementów większych
   int j = right;                     //przesuwa się w lewo, szuka elementów mniejszych
-  // cout<<left<<" piv: "<<piv<<" "<<right<<endl;
-  // cin>>a;
-  while(i<j)
+  while(true)
   {
     while(tab[i]<piv)
       i++;
@@ -20,17 +23,13 @@ int partition(int* tab, int left, int right)
     if(i<j)
       {
         swap(tab[i], tab[j]);
-        j--;
         i++;
-
-        // cout<<"swapped ";
-        // for (int i=0; i<5; i++)
-        // cout<<tab[i]<<" ";
-        // cout<<endl;
+        j--;
       }
+    else
+      return j;
+      //po prawej od piv są elementy większe od niego, po lewej - mniejsze
   }
-  //po prawej od piv są elementy większe od niego, po lewej - mniejsze
-  return j;
 }
 
 
@@ -40,12 +39,10 @@ void quick(int* tab, int left, int right)
   {
     //podziel tablicę na mniejsze części i posortuj je
     int cut = partition(tab, left, right);
-    // for (int i=0; i<=5; i++)
-    //   cout<<tab[i]<<" ";
-    // cout<<"cut in "<<cut<<endl;
-    // cin>>a;
+    if(right-left+1 == 2) //przy 2-elem. tablicy tnie po środku
+      cut = left;
     quick(tab, left, cut);
-    quick(tab, cut + 1, right);
+    quick(tab, cut+1, right);
   }
 }
 
